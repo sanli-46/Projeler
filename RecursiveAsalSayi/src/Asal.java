728154f12d990636153309ab5d2137ab97326aaf
sanli-46 <81872331+sanli-46@users.noreply.github.com>
2022-07-09 01:52:41 +0300

    asal sayı

---
+++ RecursiveAsalSayi/src/Asal.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Asal {
+
+
+
+        static  boolean asalsayi(int sayi, int i){
+            if (sayi <= 2){
+                return (sayi == 2) ? true : false;
+            }
+
+            if (sayi % i == 0){
+                return false;
+            }
+
+            if (i * i > sayi){
+                return true;
+            }
+            return asalsayi(sayi, i + 1);
+        }
+
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Bir sayıyı giriniz: ");
+            int sayi = sc.nextInt();
+
+            if (asalsayi(sayi, 2)) {
+                System.out.println(sayi + " sayısı asal sayıdır. ");
+            } else {
+                System.out.println(sayi + " sayısı asal sayı değildir.");
+            }
+
+
+        }
+}

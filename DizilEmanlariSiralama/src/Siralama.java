5ca69c1953fb47ee5020398d14eb221f3b6317ee
sanli-46 <81872331+sanli-46@users.noreply.github.com>
2022-07-14 20:24:48 +0300

    Dizi elemanı yapma

---
+++ DizilEmanlariSiralama/src/Siralama.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Siralama {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Dizi kac elemanli olacak ?  :");
+        int uzunluk=inp.nextInt();
+        int[] list1=new int[uzunluk];
+
+        System.out.println("Dizi Elemanlarini Giriniz");
+
+          for (int i = 0; i < list1.length ; i++){
+              System.out.print((i+1)+".Eleman :");
+              list1[i]=inp.nextInt();
+          }
+
+        System.out.println("Siralama :"+Arrays.toString(list1));
+    }
+}
